60738d58f3b01606ea7a5807ed6b6703ffd83552
Estados <hsuliman@gmail.com>
2023-10-20 20:12:14 +0100

    Agregación de atributos en Vehicle y Plate.

--- workshop/src/org/ulpgc/is1/model/Plate.java
+++ workshop/src/org/ulpgc/is1/model/Plate.java
@@ -1,4 +1,41 @@
 package org.ulpgc.is1.model;
 
 public class Plate {
+    private String number;
+
+    public Plate(String number) {
+        if(isValidPlate(number)){
+            this.number = number;
+        } else {
+            this.number = "XXXX";
+        }
+
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        if (isValidPlate(number)) {
+            this.number = number;
+        } else {
+            this.number = "XXXX";
+        }
+
+    }
+        public boolean isValidPlate(String number){
+            if (number.matches("\\d{4} [A-Za-z]{3}")) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+
+    @Override
+    public String toString() {
+        return "Plate{" +
+                "number='" + number + '\'' +
+                '}';
+    }
 }
--- workshop/src/org/ulpgc/is1/model/Vehicle.java
+++ workshop/src/org/ulpgc/is1/model/Vehicle.java
@@ -1,4 +1,37 @@
 package org.ulpgc.is1.model;
 
 public class Vehicle {
+    private String make;
+    private String model;
+    private Plate plate;
+
+    public Vehicle(String make, String model, Plate plate) {
+        this.make = make;
+        this.model = model;
+        this.plate = plate;
+    }
+
+    public String getMake() {
+        return make;
+    }
+
+    public void setMake(String make) {
+        this.make = make;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public Plate getPlate() {
+        return plate;
+    }
+
+    public void setPlate(Plate plate) {
+        this.plate = plate;
+    }
 }
