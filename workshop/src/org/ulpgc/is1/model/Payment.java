68c355f1480ccadaf44a6852872011a91fdda3d4
Estados <hsuliman@gmail.com>
2023-10-20 20:31:43 +0100

    Agregación de atributos en Mechanic y Payment.

--- workshop/src/org/ulpgc/is1/model/Payment.java
+++ workshop/src/org/ulpgc/is1/model/Payment.java
@@ -1,4 +1,27 @@
 package org.ulpgc.is1.model;
-
+import java.util.Date;
 public class Payment {
+    private Date date;
+    private int amount;
+
+    public Payment(Date date, int amount) {
+        this.date = date;
+        this.amount = amount;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+
+    public void setAmount(int amount) {
+        this.amount = amount;
+    }
 }
