e4111c4128eff63203786831127c4c6a32e92037
Estados <hsuliman@gmail.com>
2023-10-20 20:16:32 +0100

    Agregación de atributos en SparePart

--- workshop/src/org/ulpgc/is1/model/SparePart.java
+++ workshop/src/org/ulpgc/is1/model/SparePart.java
@@ -1,4 +1,27 @@
 package org.ulpgc.is1.model;
 
 public class SparePart {
+    private String name;
+    private int price;
+
+    public SparePart(String name, int price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
 }
